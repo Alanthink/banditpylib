15b1e28eee30ffcbdf1a43746c1168b293674090
Chester Holtz <chesterholtz@gmail.com>
2020-02-24 15:53:34 -0800

    pep8

--- learners/regretmin/decentralizedordinarylearner/fucb.py
+++ learners/regretmin/decentralizedordinarylearner/fucb.py
@@ -29,11 +29,12 @@ class FUCB(DecentralizedOrdinaryLearner):
     messages = [list(m.values())[0] for m in messages]
     arm_scores = np.array(messages).T
     armids, pulls = np.unique(arm_scores[0], return_counts=True)
-    rew = [arm_scores[1][arm_scores[0] == a].sum() for a in range(self._arm_num)]
+    rew = [arm_scores[1][arm_scores[0] == a].sum() 
+           for a in range(self._arm_num)]
 
     ucb = [rew[arm] / pulls[arm] +
-            np.sqrt(self.__alpha / pulls[arm] * np.log(len(messages) - 1))
-            for arm in range(self._arm_num)]
+           np.sqrt(self.__alpha / pulls[arm] * np.log(len(messages) - 1))
+           for arm in range(self._arm_num)]
     return np.argmax(ucb)
 
   def _learner_update(self, context, action, feedback):
