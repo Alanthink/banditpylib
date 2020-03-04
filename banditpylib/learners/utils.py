bbd634e75754140560d5e07993fb12c10f5ab041
taochao <taochao@iu.edu>
2020-03-03 20:32:31 -0500

    minor update

--- banditpylib/learners/utils.py
+++ banditpylib/learners/utils.py
@@ -5,10 +5,10 @@ Before each trial is run, a learner must be initialized with `init`. The
 parameters of `init` may not be the same for different types of learners.
 However, the first argument should always be the bandit instance since during
 the initialization, a learner may want to ask the bandit for some information.
-`reward` should return a function of the learner such that the protocol knows
-which function of the learner to call to ask for the empirical rewards.
-`regret_def` returns a string will be used by the protocol to ask the bandit for
-the correct regret.
+`rewards_def` should return a function of the learner such that the protocol
+knows which function of the learner to call to ask for the empirical rewards.
+`regret_def` returns a string which will be used by the protocol to ask the
+bandit for the correct regret.
 """
 from abc import ABC, abstractmethod
 from absl import logging
