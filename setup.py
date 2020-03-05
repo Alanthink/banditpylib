a1b8b0ecd3ac54e9991197977b4a39848253681a
taochao <taochao@iu.edu>
2020-03-04 23:18:44 -0800

    add more constraints in setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="banditpylib",
-    version="0.1.0",
+    version="0.0.1",
     author="Chester Holtz, Chao Tao",
     author_email="chholtz@eng.ucsd.edu, sdutaochao@gmail.com",
     description="A lightweight python library for bandit algorithms",
@@ -43,5 +43,11 @@ setup(
         'tabulate',
         'wcwidth',
         'zipp'
-    ]
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
 )
