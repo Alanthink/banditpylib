300cad96f544b45b799f28fa1f7184a5ccdb1e9f
taochao <taochao@iu.edu>
2020-08-22 00:13:29 -0400

    add ucb leaner and example

---
+++ banditpylib/learners/ordinary_learner/ucb_test.py
@@ -0,0 +1,25 @@
+from unittest.mock import MagicMock
+
+import numpy as np
+
+from .ucb import UCB
+
+
+class TestEpsGreedy:
+  """Test epsilon greedy policy"""
+
+  def test_simple_run(self):
+    arm_num = 5
+    horizon = 30
+    learner = UCB(arm_num=arm_num, horizon=horizon)
+    mock_ucb = np.array([1.2, 1, 1, 1, 1])
+    learner.UCB = MagicMock(return_value=mock_ucb)
+    learner.reset()
+    # during the first 5 time steps, each arm is pulled once
+    for time in range(1, arm_num + 1):
+      assert learner.actions() == [((time-1) % arm_num, 1)]
+      learner.update(([np.array([0])], ))
+    # for the left time steps, arm 0 is always the choice
+    for _ in range(arm_num + 1, horizon + 1):
+      assert learner.actions() == [(0, 1)]
+      learner.update(([np.array([0])], ))
--- banditpylib/learners/risk_aware_mnl_learner/ucb.py
+++ banditpylib/learners/risk_aware_mnl_learner/ucb.py
@@ -1,5 +1,3 @@
-import copy
-
 import numpy as np
 
 from banditpylib.bandits import search_best_assortment, Reward
@@ -7,11 +5,7 @@ from .utils import RiskAwareMNLLearner
 
 
 class RiskAwareUCB(RiskAwareMNLLearner):
-  """Risk-aware UCB policy
-
-  Product 0 is reserved for non-purchase. And it is assumed that the abstraction
-  parameter for non-purchase is 1.
-  """
+  """Risk-aware UCB policy"""
   def __init__(self,
                revenues: np.ndarray,
                horizon: int,
@@ -26,20 +20,8 @@ class RiskAwareUCB(RiskAwareMNLLearner):
       card_limit: cardinality constraint
       name: alias name for the learner
     """
-    # product 0 is reserved for non-purchase
-    self.__product_num = len(revenues) - 1
-    if self.__product_num < 2:
-      raise Exception('Number of products %d is less then 2!' %
-                      self.__product_num)
-    self.__revenues = revenues
-    self.__card_limit = card_limit
-    if horizon < self.__product_num:
-      raise Exception('Horizon %d is less than number of products %d!' % \
-          (horizon, self.__product_num))
-    self._horizon = horizon
-    self.__reward = copy.deepcopy(reward)
-    self.__reward.set_revenues(self.__revenues)
     self.__name = name if name else 'risk_aware_ucb'
+    super().__init__(revenues, horizon, reward, card_limit)
 
   @property
   def name(self):
@@ -52,10 +34,10 @@ class RiskAwareUCB(RiskAwareMNLLearner):
     self.__episode = 1
     # number of episodes a product is served until the current episode
     # (exclusive)
-    self.__serving_times = np.zeros(self.__product_num + 1)
+    self.__serving_times = np.zeros(self.product_num() + 1)
     # number of times the customer chooses a product until the current episode
     # (exclusive)
-    self.__customer_choices = np.zeros(self.__product_num + 1)
+    self.__customer_choices = np.zeros(self.product_num() + 1)
     self.__last_actions = None
     self.__last_feedback = None
 
@@ -67,7 +49,7 @@ class RiskAwareUCB(RiskAwareMNLLearner):
     # average choices by the customer per episode
     avg_choices = self.__customer_choices / self.__serving_times
     # temperary result
-    tmp_result = 48 * np.log(np.sqrt(self.__product_num) * self.__episode +
+    tmp_result = 48 * np.log(np.sqrt(self.product_num()) * self.__episode +
                              1) / avg_choices
     ucb = avg_choices + np.sqrt(avg_choices * tmp_result) + tmp_result
     ucb[np.isnan(ucb)] = 1
@@ -75,7 +57,8 @@ class RiskAwareUCB(RiskAwareMNLLearner):
     return ucb
 
   def actions(self, context=None):
-    if self.__time > self._horizon:
+    del context
+    if self.__time > self.horizon():
       self.__last_actions = None
       return self.__last_actions
     # check if last observation is not a non-purchase
@@ -83,12 +66,12 @@ class RiskAwareUCB(RiskAwareMNLLearner):
       return self.__last_actions
     # When a non-purchase observation happens, a new episode is started and
     # a new assortment to be served is calculated
-    self.__reward.set_abstraction_params(self.UCB())
+    self.reward().set_abstraction_params(self.UCB())
     # calculate assortment with the maximum reward using optimistic abstraction
     # parameters
-    _, best_assortment = search_best_assortment(product_num=self.__product_num,
-                                                reward=self.__reward,
-                                                card_limit=self.__card_limit)
+    _, best_assortment = search_best_assortment(product_num=self.product_num(),
+                                                reward=self.reward(),
+                                                card_limit=self.card_limit())
     self.__last_actions = [(best_assortment, 1)]
     return self.__last_actions
 
