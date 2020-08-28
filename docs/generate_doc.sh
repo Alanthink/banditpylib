cde493b14c37917ab6d167490d5f09456e635ac1
taochao <taochao@iu.edu>
2020-08-28 00:55:33 -0400

    add website to submodule

--- docs/generate_doc.sh
+++ docs/generate_doc.sh
@@ -1,5 +1,29 @@
-rm -rf build
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+rm -rf build/*
 
 sphinx-apidoc -M -e -f -o ./source ../banditpylib/
 
 make html
+
+printf "\033[0;32mDeploying updated documentation to GitHub...\033[0m\n"
+
+cp -r build/html/* site/
+
+Commit () {
+  # Add changes to git.
+  git add .
+  # Commit changes.
+  msg="rebuilding site $(date)"
+  if [ -n "$*" ]; then
+    msg="$*"
+  fi
+  git commit -m "$msg" || true
+}
+
+# Go to site folder
+cd site
+Commit
