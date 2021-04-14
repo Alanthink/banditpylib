562c40449c7283607ccf7c0a7b8864b930851479
taochao <taochao@iu.edu>
2021-04-14 12:33:42 -0400

    add one field in DataItem

--- banditpylib/data_pb2.py
+++ banditpylib/data_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='data.proto',
   package='banditpylib',
   syntax='proto3',
-  serialized_pb=_b('\n\ndata.proto\x12\x0b\x62\x61nditpylib\"\x1e\n\x03\x41rm\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0b\n\x03ids\x18\x02 \x03(\x05\"<\n\x0c\x41rmPullsPair\x12\x1d\n\x03\x61rm\x18\x01 \x01(\x0b\x32\x10.banditpylib.Arm\x12\r\n\x05pulls\x18\x02 \x01(\x05\"=\n\x07\x41\x63tions\x12\x32\n\x0f\x61rm_pulls_pairs\x18\x01 \x03(\x0b\x32\x19.banditpylib.ArmPullsPair\"\\\n\x0e\x41rmRewardsPair\x12\x1d\n\x03\x61rm\x18\x01 \x01(\x0b\x32\x10.banditpylib.Arm\x12\x0f\n\x07rewards\x18\x02 \x03(\x02\x12\x1a\n\x12\x63ustomer_feedbacks\x18\x03 \x03(\x05\"B\n\x08\x46\x65\x65\x64\x62\x61\x63k\x12\x36\n\x11\x61rm_rewards_pairs\x18\x01 \x03(\x0b\x32\x1b.banditpylib.ArmRewardsPair\"b\n\x08\x44\x61taItem\x12\x0e\n\x06\x62\x61ndit\x18\x01 \x01(\t\x12\x0f\n\x07learner\x18\x02 \x01(\t\x12\x0e\n\x06rounds\x18\x03 \x01(\x05\x12\x15\n\rtotal_actions\x18\x04 \x01(\x05\x12\x0e\n\x06regret\x18\x05 \x01(\x02\"2\n\x05Trial\x12)\n\ndata_items\x18\x01 \x03(\x0b\x32\x15.banditpylib.DataItemb\x06proto3')
+  serialized_pb=_b('\n\ndata.proto\x12\x0b\x62\x61nditpylib\"\x1e\n\x03\x41rm\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0b\n\x03ids\x18\x02 \x03(\x05\"<\n\x0c\x41rmPullsPair\x12\x1d\n\x03\x61rm\x18\x01 \x01(\x0b\x32\x10.banditpylib.Arm\x12\r\n\x05pulls\x18\x02 \x01(\x05\"=\n\x07\x41\x63tions\x12\x32\n\x0f\x61rm_pulls_pairs\x18\x01 \x03(\x0b\x32\x19.banditpylib.ArmPullsPair\"\\\n\x0e\x41rmRewardsPair\x12\x1d\n\x03\x61rm\x18\x01 \x01(\x0b\x32\x10.banditpylib.Arm\x12\x0f\n\x07rewards\x18\x02 \x03(\x02\x12\x1a\n\x12\x63ustomer_feedbacks\x18\x03 \x03(\x05\"B\n\x08\x46\x65\x65\x64\x62\x61\x63k\x12\x36\n\x11\x61rm_rewards_pairs\x18\x01 \x03(\x0b\x32\x1b.banditpylib.ArmRewardsPair\"q\n\x08\x44\x61taItem\x12\x0e\n\x06\x62\x61ndit\x18\x01 \x01(\t\x12\x0f\n\x07learner\x18\x02 \x01(\t\x12\x0e\n\x06rounds\x18\x03 \x01(\x05\x12\x15\n\rtotal_actions\x18\x04 \x01(\x05\x12\x0e\n\x06regret\x18\x05 \x01(\x02\x12\r\n\x05other\x18\x06 \x01(\x02\"2\n\x05Trial\x12)\n\ndata_items\x18\x01 \x03(\x0b\x32\x15.banditpylib.DataItemb\x06proto3')
 )
 
 
@@ -250,6 +250,13 @@ _DATAITEM = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='other', full_name='banditpylib.DataItem.other', index=5,
+      number=6, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -263,7 +270,7 @@ _DATAITEM = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=346,
-  serialized_end=444,
+  serialized_end=459,
 )
 
 
@@ -293,8 +300,8 @@ _TRIAL = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=446,
-  serialized_end=496,
+  serialized_start=461,
+  serialized_end=511,
 )
 
 _ARMPULLSPAIR.fields_by_name['arm'].message_type = _ARM
