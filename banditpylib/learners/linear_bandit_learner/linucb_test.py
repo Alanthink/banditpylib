f8a50057e6c188744a1b23ac45c424c07a40512b
sheelfshah <sheelfshah@gmail.com>
2021-05-18 00:28:03 +0530

    linucb test added

--- banditpylib/learners/linear_bandit_learner/linucb_test.py
+++ banditpylib/learners/linear_bandit_learner/linucb_test.py
@@ -1,34 +1,47 @@
-import matplotlib.pyplot as plt
+from unittest.mock import MagicMock
+
+import google.protobuf.text_format as text_format
+
 import numpy as np
-import seaborn as sns
-sns.set(style="darkgrid")
 
+from banditpylib.data_pb2 import Actions, Feedback
 from banditpylib.bandits import LinearBandit
-from banditpylib.protocols import SinglePlayerProtocol, trial_data_messages_to_dict
-from banditpylib.learners.linear_bandit_learner import LinUCB
+from .linucb import LinUCB
 
-horizon = 2000
-num_arms = 100
-len_feature = 5
-features = []
 
-for _ in range(num_arms):
-    features.append(np.random.normal(0, size=len_feature))
-    features[-1] = features[-1] / np.linalg.norm(features[-1])
+class TestLinUCB:
+    """Test LinUCB Policy"""
 
-theta = np.random.normal(0, size=len_feature)
+    def test_simple_run(self):
+        arm_num = 5
+        horizon = 10
+        features = [np.array([1, 0]), np.array([1, 0]), np.array(
+            [1, 0]), np.array([1, 0]), np.array([0, 1])]
+        learner = LinUCB(arm_num, features, 0.1, 1e-3)
+        learner.reset()
+        mock_ucb = np.array([1.2, 1, 1, 1, 1])
+        # pylint: disable=protected-access
+        learner._LinUCB__LinUCB = MagicMock(return_value=mock_ucb)
 
-bandit = LinearBandit(features, theta)
-learners = [LinUCB(num_arms, features, 1 / horizon, 1e-3)]
-
-intermediate_regrets = list(range(0, horizon + 1, 50))
-
-game = SinglePlayerProtocol(
-    bandit, learners,
-    intermediate_regrets=intermediate_regrets, horizon=horizon)
-
-game.play(trials=100, output_filename="temp_output.txt")
-
-data_df = trial_data_messages_to_dict("temp_output.txt")
-sns.lineplot(x='total_actions', y='regret', hue='learner', data=data_df)
-plt.show()
+        # Always 0th arm is picked
+        # not the most efficient test
+        for time in range(1, horizon + 1):
+            assert learner.actions().SerializeToString() == text_format.Parse(
+                """
+            arm_pulls_pairs <
+              arm <
+                id: 0
+              >
+              pulls: 1
+            >
+            """, Actions()).SerializeToString()
+            learner.update(
+                text_format.Parse(
+                    """
+            arm_rewards_pairs <
+              arm <
+                id: 0
+              >
+              rewards: 0
+            >
+            """, Feedback()))
