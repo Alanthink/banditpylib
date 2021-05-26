51227244c9c99aca76da2af13b1a029e22fbab27
sheelfshah <sheelfshah@gmail.com>
2021-05-26 20:57:30 +0530

    make lint and make fix attempted

--- banditpylib/learners/linear_bandit_learner/linucb_test.py
+++ banditpylib/learners/linear_bandit_learner/linucb_test.py
@@ -10,24 +10,28 @@ from .linucb import LinUCB
 
 
 class TestLinUCB:
-    """Test LinUCB Policy"""
+  """Test LinUCB Policy"""
+  def test_simple_run(self):
+    arm_num = 5
+    horizon = 10
+    features = [
+        np.array([1, 0]),
+        np.array([1, 0]),
+        np.array([1, 0]),
+        np.array([1, 0]),
+        np.array([0, 1])
+    ]
+    learner = LinUCB(arm_num, features, 0.1, 1e-3)
+    learner.reset()
+    mock_ucb = np.array([1.2, 1, 1, 1, 1])
+    # pylint: disable=protected-access
+    learner._LinUCB__LinUCB = MagicMock(return_value=mock_ucb)
 
-    def test_simple_run(self):
-        arm_num = 5
-        horizon = 10
-        features = [np.array([1, 0]), np.array([1, 0]), np.array(
-            [1, 0]), np.array([1, 0]), np.array([0, 1])]
-        learner = LinUCB(arm_num, features, 0.1, 1e-3)
-        learner.reset()
-        mock_ucb = np.array([1.2, 1, 1, 1, 1])
-        # pylint: disable=protected-access
-        learner._LinUCB__LinUCB = MagicMock(return_value=mock_ucb)
-
-        # Always 0th arm is picked
-        # not the most efficient test
-        for time in range(1, horizon + 1):
-            assert learner.actions().SerializeToString() == text_format.Parse(
-                """
+    # Always 0th arm is picked
+    # not the most efficient test
+    for time in range(1, horizon + 1):
+      assert learner.actions().SerializeToString() == text_format.Parse(
+          """
             arm_pulls_pairs <
               arm <
                 id: 0
@@ -35,9 +39,9 @@ class TestLinUCB:
               pulls: 1
             >
             """, Actions()).SerializeToString()
-            learner.update(
-                text_format.Parse(
-                    """
+      learner.update(
+          text_format.Parse(
+              """
             arm_rewards_pairs <
               arm <
                 id: 0
--- banditpylib/learners/linear_bandit_learner/utils.py
+++ banditpylib/learners/linear_bandit_learner/utils.py
@@ -5,28 +5,27 @@ from banditpylib.learners import Learner
 
 
 class LinearBanditLearner(Learner):
-    """Abstract class for learners playing with linear bandit"""
-
-    def __init__(self, arm_num: int, name: Optional[str]):
-        """
+  """Abstract class for learners playing with linear bandit"""
+  def __init__(self, arm_num: int, name: Optional[str]):
+    """
         Args:
           arm_num: number of arms
           budget: total number of pulls
           name: alias name
         """
-        super().__init__(name)
-        if arm_num < 2:
-            raise ValueError('Number of arms is expected at least 2. Got %d.' %
-                             arm_num)
-        self.__arm_num = arm_num
+    super().__init__(name)
+    if arm_num < 2:
+      raise ValueError('Number of arms is expected at least 2. Got %d.' %
+                       arm_num)
+    self.__arm_num = arm_num
 
-    @property
-    def running_environment(self) -> type:
-        return LinearBandit
+  @property
+  def running_environment(self) -> type:
+    return LinearBandit
 
-    def arm_num(self) -> int:
-        """
+  def arm_num(self) -> int:
+    """
         Returns:
           number of arms
         """
-        return self.__arm_num
+    return self.__arm_num
